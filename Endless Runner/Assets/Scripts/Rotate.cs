13f44fe5101abdfea0edc8e03c74d51ace4eaa4d
FadiMasannat <60350273+FadiMasannat@users.noreply.github.com>
2020-04-10 16:17:11 +0300

    something

---
+++ Endless Runner/Assets/Scripts/Rotate.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("a"))
+        {
+            transform.Rotate(new Vector3(0f, -10f, 0f) * Time.deltaTime);
+        }
+        if (Input.GetKey("d"))
+        {
+            transform.Rotate(new Vector3(0f, -10f, 0f) * Time.deltaTime);
+        }
+    }
+}

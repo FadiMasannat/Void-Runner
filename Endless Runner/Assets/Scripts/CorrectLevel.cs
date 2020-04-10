d98f9f536456f12688ea9d40b0d1844bdc6042ee
FadiMasannat <60350273+FadiMasannat@users.noreply.github.com>
2020-04-10 13:18:34 +0300

    Finalized the start

---
+++ Endless Runner/Assets/Scripts/CorrectLevel.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class CorrectLevel : MonoBehaviour
+{
+
+    public Text selectedlevel;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        selectedlevel.text = "Level " + SceneManager.GetActiveScene().buildIndex;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Endless Runner/Assets/Scripts/credits.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class credits : MonoBehaviour
+{
+
+    public void Quit()
+    {
+        Application.Quit();
+        Debug.Log("Quit");
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
